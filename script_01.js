141f2917cdde46d7065be2258402032b565605c2
Peter <schmidt.multimedia@gmail.com>
2021-08-03 12:07:45 +0200

    02 / Theorie: Array

--- script_01.js
+++ script_01.js
@@ -1,12 +1,27 @@
 /* Die Satzbau-Maschine | Arrays */
 
+/* Theorie: Array */
+
+let arr;
+
+arr = new Array(); // Konstruktor
+arr = []; // Literal
+arr = [2,4,7,12];
+arr = [true,false];
+arr = ["Ich","bin","Max","Mütze"];
+
+ausgabe(arr);
+ausgabe(arr.length);
+ausgabe(arr[0]); // Index, begint IMMER mit 0
+ausgabe(arr[3]);
+ausgabe(arr[arr.length-1]);
 
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 
 // Modul: Satz-String zusammensetzen
-ausgabe(getSentence("Ich","bin","Max","Mütze"));
+// ausgabe(getSentence("Ich","bin","Max","Mütze"));
 function getSentence(word1,word2,word3,word4) {
     const gap = " ";
     const punct = "."
