13270fe9a1e7c1a73bd88468d949e12a126c6905
Peter <schmidt.multimedia@gmail.com>
2021-08-03 12:27:04 +0200

    03 / Funktionalität mit Array 1

--- script_01.js
+++ script_01.js
@@ -2,20 +2,37 @@
 
 /* Theorie: Array */
 
-let arr;
+// let arr;
 
-arr = new Array(); // Konstruktor
-arr = []; // Literal
-arr = [2,4,7,12];
-arr = [true,false];
-arr = ["Ich","bin","Max","Mütze"];
+// arr = new Array(); // Konstruktor
+// arr = []; // Literal
+// arr = [2,4,7,12];
+// arr = [true,false];
+// arr = ["Ich","bin","Max","Mütze"];
 
-ausgabe(arr);
-ausgabe(arr.length);
-ausgabe(arr[0]); // Index, begint IMMER mit 0
-ausgabe(arr[3]);
-ausgabe(arr[arr.length-1]);
+// ausgabe(arr);
+// ausgabe(arr.length);
+// ausgabe(arr[0]); // Index, begint IMMER mit 0
+// ausgabe(arr[3]);
+// ausgabe(arr[arr.length-1]);
 
+
+/*** 02a. Funktionalität mit Array 1  */
+// Kritik | Überlegung
+// word1,word2, .... word100  :: Semantische Struktur
+// arr[0],arr[1] ... arr[n] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation  semantisches Problem --> numerisches Problem
+// ausgabe(getSentenceArr(["Ich","bin","Max","Mütze"]));
+function getSentenceArr(arr) {
+    const gap = " ";
+    const punct = "."
+    const str = arr[0] + gap + 
+                arr[1] + gap +
+                arr[2] + gap +
+                arr[3] +
+                punct;
+    return str; 
+}
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
